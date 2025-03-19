c4d39df3d38f53ffec70c715abd89b1ed463c4c4
Antonio Carlos <acbarbosawhat@gmail.com>
2025-03-19 10:22:04 -0300

    Update projeto.py
    Correcao

--- projeto.py
+++ projeto.py
@@ -11,53 +11,56 @@ df = pd.read_excel('dados3.xlsx')
 fin = pd.read_excel('financeiro.xlsx')
 df = df.sort_values("Cooperativa")
 
-filtro_Cooperativa = st.sidebar.multiselect('Filtrar por Escritórios', df["Cooperativa"].unique()) 
+filtro_Cooperativa = st.sidebar.multiselect('Filtrar Adesões por Escritórios', df["Cooperativa"].unique()) 
 df_filtered = df[df["Cooperativa"].isin(filtro_Cooperativa)] if  filtro_Cooperativa else df
 
-st.title("PAINEL - ADESÕES e FINANCEIRO - MÊS JANEIRO 2025")
+filtro_financeiro = st.sidebar.multiselect('Filtro Recebimentos por Escritórios', fin["Cooperativa"].unique()) 
+fin_filtered = fin[fin["Cooperativa"].isin(filtro_financeiro)] if  filtro_financeiro else fin
+
+st.title("PAINEL - ADESÕES e FINANCEIRO - MÊS 2025")
 
 col1, col2 , col3, col4 = st.columns(4)
 col5, col6, col7, col8 = st.columns(4)
 
 adesoes = df['Situacao'].count()
 with col1:
-     st.metric('Quant. de Adesões', round(adesoes))
+     st.metric("Quant. de Adesões", round(adesoes),border=True)
 
-novos = df['Classificacao'].isin(['NOVO']) .sum()
+novos = df['Classificacao'].isin(['NOVO']).sum()
 with col2:
-     st.metric('Adesoes Novas', round(novos))
+     st.metric("Adesoes Novas", round(novos), border=True)
 
-renovacoes = df['Classificacao'].isin(['RENOVAÇÃO']) .sum()
+renovacoes = df['Classificacao'].isin(['RENOVAÇÃO']).sum()
 with col3:
-     st.metric('Renovacoes', round(renovacoes))
+     st.metric('Renovacoes', round(renovacoes), border=True)
 
 parcela = df['Parcela'].sum()
 with col4:
-     st.metric('Soma das Parcelas', round(parcela, 2))
+     st.metric('Soma das Parcelas', round(parcela, 2), border=True)
 
 media = df['Parcela'].mean()
 with col5:
-     st.metric('Ticket Médio', round(media, 2))
+     st.metric('Ticket Médio', round(media, 2), border=True)
 
 auto = df.loc[df['Tipo'] =="AUTOMOVEL"]['Parcela'].mean()
 with col6:
-     st.metric('Ticket Auto', round(auto,2 ))
+     st.metric('Ticket Auto', round(auto,2 ), border=True)
 
 moto = df.loc[df['Tipo'] =="MOTOCICLETA"]['Parcela'].mean()
 with col7:
-     st.metric('Ticket Moto', round(moto, 2))
+     st.metric('Ticket Moto', round(moto, 2), border=True)
 
 recebido = fin['Recebido'].sum()
 with col8:
-     st.metric('Recebido Mês', round(recebido, 2))
+     st.metric('Recebido Mês', round(recebido, 2),border=True )
 
 st.divider()
 
+ 
 st.write("Situação Geral das Adesões dos Escritórios")
 st.bar_chart(df, x="Cooperativa", y="Situacao", color="Situacao", stack="center")
 
 st.divider()
-
 
 
 cooperativa = px.bar(df_filtered, x= "Cooperativa", y= "Situacao",color="Situacao",barmode="group", title= "Cooperativas")
@@ -65,6 +68,12 @@ st.plotly_chart(cooperativa, use_container_width=True)
 
 st.divider()
 
+ 
+ticket = df_filtered.groupby(["Cooperativa", "Tipo"])["Parcela"].mean().round(2).reset_index()        
+fig_novo = px.bar(ticket, x= "Cooperativa", y= "Tipo", color="Tipo", barmode="group",text="Parcela", title= " TICKET MÉDIO POR ESCRITÓRIO")
+st.plotly_chart(fig_novo) 
+
+st.divider()
 
 adesoes = df_filtered.groupby(["Cooperativa", "Classificacao"])["Nome"].count().reset_index()
 classificacao = px.bar(adesoes, x= "Cooperativa", y= "Classificacao",barmode="group",text="Nome",color="Classificacao", title= "Adesões por Cooperativas")
@@ -140,31 +149,32 @@ st.divider()
 st.markdown(''':blue[PAINEL DE RECEBIMENTOS MENSAL]''')
 
 
-boleto = fin.groupby(fin["Tipo_Boleto"])["Recebido"].sum().reset_index()
+boleto = fin_filtered.groupby(["Tipo_Boleto"])["Recebido"].sum().reset_index()
 fig_tipo = px.bar(boleto, x= "Tipo_Boleto", y= "Recebido", barmode="group",text_auto=True, title= "Recebido por Tipo de Boleto")
 st.plotly_chart(fig_tipo, use_container_width=True) 
 st.divider()
 
-data = fin.groupby(fin["Data_Recebido"])["Recebido"].sum().reset_index()
+data = fin_filtered.groupby(["Data_Recebido"])["Recebido"].sum().reset_index()
 fig_tipo = px.bar(data, x= "Data_Recebido", y= "Recebido", barmode="group",text_auto=True, title= "Datas e Recebimentos")
 st.plotly_chart(fig_tipo, use_container_width=True) 
 st.divider()
 
-treemap = fin[["Cooperativa", "Recebido"]].groupby(by=["Cooperativa"])["Recebido"].sum().reset_index()
+treemap = fin_filtered[["Cooperativa", "Recebido"]].groupby(by=["Cooperativa"])["Recebido"].sum().reset_index()
 fig2 = px.treemap(treemap, path=["Cooperativa","Recebido"], values = "Recebido", hover_data=["Cooperativa"],
                   color="Cooperativa", title="Escritórios e Recebimentos", height=700, width=600)
 fig2.update_traces(textinfo="value")
 st.plotly_chart(fig2)
 st.divider()
 
-voluntario = fin.groupby(fin["Voluntario"])["Recebido"].sum().reset_index()
+voluntario = fin_filtered.groupby(["Voluntario"])["Recebido"].sum().reset_index()
 fig_tipo = px.bar(voluntario, x= "Voluntario", y= "Recebido", barmode="group",text_auto=True, title= "Recebido por Voluntário")
 st.plotly_chart(fig_tipo, use_container_width=True) 
 st.divider()
 
-baixa = fin.groupby(fin["Tipo_Baixa"])[["Usuario"]].count().reset_index()
+baixa = fin_filtered.groupby(["Tipo_Baixa"])[["Usuario"]].count().reset_index()
 fig = px.pie(baixa, values='Usuario', names='Tipo_Baixa', title='Tipo da Baixa dos Boletos')
 fig.update_layout(legend_title="Tipo_Baixa", legend_y=0.9)
 fig.update_traces(textinfo='percent+label', textposition='inside')
 st.plotly_chart(fig, use_container_width=True)
+
 
